e187921df699b3c4a28c27af8997e0e0cf5e3120
Geoff Bacon <bacon@berkeley.edu>
2017-08-10 11:01:25 -0700

    update version information

--- eval/version.py
+++ eval/version.py
@@ -26,23 +26,19 @@ CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Topic :: Scientific/Engineering"]
 
 # Description should be a one-liner:
-description = "eval: a template for small scientific Python projects"
+description = "eval: evaluating semantic representations"
 # Long description will go up on the pypi page
 long_description = """
 
-Shablona
+Eval
 ========
-Shablona is a template project for small scientific Python projects.
+Eval is a project for evaluating learnt semantic representations.
 
-It contains software implementations of an analysis of some simple data, but
-more importantly, it contains infrastructure for testing, documentation,
-continuous integration and deployment, which can be easily adapted
-to use in other projects.
-
-To get started using these components in your own software, please go to the
+It contains implementations of both intrinsic and extrinsic evaluation 
+methods and common datasets. For more information, please see the
 repository README_.
 
-.. _README: https://github.com/uwescience/eval/blob/master/README.md
+.. _README: https://github.com/geoffbacon/eval/blob/master/README.md
 
 License
 =======
@@ -52,20 +48,19 @@ for usage, and a DISCLAIMER OF ALL WARRANTIES.
 
 All trademarks referenced herein are property of their respective holders.
 
-Copyright (c) 2015--, Ariel Rokem, The University of Washington
-eScience Institute.
+Copyright (c) 2017--, Geoff Bacon.
 """
 
 NAME = "eval"
-MAINTAINER = "Ariel Rokem"
-MAINTAINER_EMAIL = "arokem@gmail.com"
+MAINTAINER = "Geoff Bacon"
+MAINTAINER_EMAIL = "bacon@berkeley.edu"
 DESCRIPTION = description
 LONG_DESCRIPTION = long_description
-URL = "http://github.com/uwescience/eval"
+URL = "http://github.com/geoffbacon/eval"
 DOWNLOAD_URL = ""
 LICENSE = "MIT"
-AUTHOR = "Ariel Rokem"
-AUTHOR_EMAIL = "arokem@gmail.com"
+AUTHOR = "Geoff Bacon"
+AUTHOR_EMAIL = "bacon@berkeley.edu"
 PLATFORMS = "OS Independent"
 MAJOR = _version_major
 MINOR = _version_minor
